0d9ecf44102b9b3d9262abb401e9b6a95a7e25a1
tchongwang bakayoko <tchongwangbakayoko619@gmail.com>
2026-07-17 14:20:42 +0100

    first commit

---
+++ gather/conftest.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from gather.users.tests.factories import UserFactory
+
+if TYPE_CHECKING:
+    from gather.users.models import User
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory.create()
